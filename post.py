be1194f2e09a9b7c22727a721f9b5349fab56435
BBBaekSeung <128893466+BBBaekSeung@users.noreply.github.com>
2024-12-11 17:07:45 +0900

    post.py

--- post.py
+++ post.py
@@ -22,7 +22,6 @@ if st.button("생성하기"):
         )
         st.success(response.choices[0].message.content)
 
-        # 제품 홍보 포스터 생성
         with st.spinner("열심히 생성하고 있어요!"):
             image_response = client.images.generate(
                 model="dall-e-3",
@@ -31,4 +30,4 @@ if st.button("생성하기"):
                 n=1,
             )
         image_url = image_response.data[0].url
-        st.image(image_url)  # 생성된 포스터 출력
+        st.image(image_url) 
